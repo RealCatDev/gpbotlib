e11239e87ed13a7936d2615e3912cb59cc340164
RealCatDev <real.catdev@gmail.com>
2025-04-23 15:25:29 +0200

    Renamed event stack to event queue

--- include/gpbotlib/event.h
+++ include/gpbotlib/event.h
@@ -5,37 +5,35 @@
 #include <gpbotlib/result.h>
 #include <stdbool.h>
 
-typedef enum {
+typedef enum Gp_Event_Type {
+  GP_EVENT_NONE = 0,
   GP_EVENT_JOIN,
   GP_EVENT_DISCONNECT,
-  GP_EVENT_NONE,
+
+  COUNT_GP_EVENT_TYPES
 } Gp_Event_Type;
 
-typedef struct {} Gp_Event_None;
-typedef struct {} Gp_Event_Join;
-typedef struct {
+typedef struct Gp_Event_Disconnect {
   Gp_String reason;
 } Gp_Event_Disconnect;
 
-typedef struct {
+typedef struct Gp_Event {
   Gp_Event_Type type;
-  union Gp_Event_Union {
-    Gp_Event_None none;
-    Gp_Event_Join join;
+  union Gp_Event_As {
     Gp_Event_Disconnect disconnect;
-  } event;
+  } as;
 } Gp_Event;
 
-typedef struct {
+typedef struct Gp_Event_Queue {
   Gp_Event *allocatedStart;
   uint8_t offset;
   uint16_t used; // this is without counting offset
   uint16_t allocated;
-} Gp_Event_Stack;
+} Gp_Event_Queue;
 
-Gp_Result _gp_init_event_stack(Gp_Event_Stack *stack);
-Gp_Result _gp_add_event(Gp_Event_Stack *stack, Gp_Event event);
-Gp_Result gp_poll_event(Gp_Event_Stack *stack, Gp_Event *event);
-Gp_Result _gp_free_event_stack(Gp_Event_Stack *stack);
+Gp_Result _gp_init_event_queue(Gp_Event_Queue *queue);
+Gp_Result _gp_push_event(Gp_Event_Queue *queue, Gp_Event event);
+Gp_Result gp_poll_event(Gp_Event_Queue *queue, Gp_Event *event);
+Gp_Result _gp_free_event_queue(Gp_Event_Queue *queue);
 
 #endif // _GPBOTLIB_EVENT_H
--- src/event.c
+++ src/event.c
@@ -4,50 +4,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GP_EVENT_STACK_CHUNK_SIZE 256
+#define GP_EVENT_QUEUE_CHUNK_SIZE 256
 
-Gp_Result _gp_init_event_stack(Gp_Event_Stack *stack) {
-  stack->allocatedStart = malloc(GP_EVENT_STACK_CHUNK_SIZE * sizeof(Gp_Event));
-  stack->offset = 0;
-  stack->used = 0;
-  stack->allocated = GP_EVENT_STACK_CHUNK_SIZE;
-  
+Gp_Result _gp_init_event_queue(Gp_Event_Queue *queue) {
+  if (!queue) return GP_INVALID_ARGS;
+
+  queue->allocatedStart = malloc(GP_EVENT_QUEUE_CHUNK_SIZE * sizeof(Gp_Event));
+  if (!queue->allocatedStart) return GP_BUY_MORE_RAM;
+
+  queue->offset = 0;
+  queue->used = 0;
+  queue->allocated = GP_EVENT_QUEUE_CHUNK_SIZE;
+
   return GP_SUCCESS;
 }
 
-Gp_Result _gp_add_event(Gp_Event_Stack *stack, Gp_Event event) {
-  if(stack->used + stack->offset >= stack->allocated) {
-    Gp_Event *resized = realloc(stack->allocatedStart, (stack->allocated + GP_EVENT_STACK_CHUNK_SIZE) * sizeof(Gp_Event));
-    if (!resized)
-      return GP_BUY_MORE_RAM;
-    stack->allocatedStart = resized;
-    stack->allocated += GP_EVENT_STACK_CHUNK_SIZE;
-  }
-  stack->allocatedStart[(stack->used++) + stack->offset] = event;
-}
+Gp_Result _gp_push_event(Gp_Event_Queue *queue, Gp_Event event) {
+  if (!queue) return GP_INVALID_ARGS;
+  if (!queue->used) queue->offset = 0;
 
-Gp_Result gp_poll_event(Gp_Event_Stack *stack, Gp_Event *event) {  
-  if (stack->used == 0) {
-    *event = (Gp_Event){
-      .type = GP_EVENT_NONE,
-      .event = (Gp_Event_None){}
-    };
-    return GP_SUCCESS;
+  if (queue->used + queue->offset >= queue->allocated) {
+    queue->allocated += GP_EVENT_QUEUE_CHUNK_SIZE;
+    queue->allocatedStart = realloc(queue->allocatedStart, queue->allocated * sizeof(Gp_Event));
+    if (!queue->allocatedStart) return GP_BUY_MORE_RAM;
   }
 
-  *event  = stack->allocatedStart[stack->offset++];
-  if(stack->offset == GP_EVENT_STACK_CHUNK_SIZE) {
-    memmove(stack->allocatedStart, stack->allocatedStart + stack->offset * sizeof(Gp_Event), stack->used * sizeof(Gp_Event));
-    Gp_Event *resized = realloc(stack->allocatedStart, (stack->allocated - stack->offset));
-    if(!resized)
-      return GP_BUY_MORE_RAM; // Or less :/
-    stack->allocatedStart = resized;
-  }
-  
+  queue->allocatedStart[queue->used++ + queue->offset] = event;
+
   return GP_SUCCESS;
 }
 
-Gp_Result _gp_free_event_stack(Gp_Event_Stack *stack) {
-  free(stack->allocatedStart);
-  stack->allocatedStart == NULL;
+Gp_Result gp_poll_event(Gp_Event_Queue *queue, Gp_Event *event) {
+  if (!queue) return GP_INVALID_ARGS;
+  if (!queue->used) return GP_SUCCESS;
+  if (event) *event = queue->allocatedStart[--queue->used, queue->offset++];
+
+  return GP_TRUE;
+}
+
+Gp_Result _gp_free_event_queue(Gp_Event_Queue *queue) {
+  if (!queue) return GP_INVALID_ARGS;
+  free(queue->allocatedStart);
+  return (queue->allocatedStart = NULL, GP_SUCCESS);
 }
