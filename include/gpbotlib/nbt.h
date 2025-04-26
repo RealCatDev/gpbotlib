856cb1af9c5476d374d5212379aa0b7ced2c431e
RealCatDev <real.catdev@gmail.com>
2025-04-26 20:54:18 +0200

    Fixed uint16_t's issues

--- include/gpbotlib/nbt.h
+++ include/gpbotlib/nbt.h
@@ -1,9 +1,11 @@
 #ifndef   _GPBOTLIB_NBT_H_
 #define   _GPBOTLIB_NBT_H_
 
-#include <stdint.h>
-#include <gpbotlib/buffer.h>
 #include <gpbotlib/result.h>
+#include <gpbotlib/buffer.h>
+
+#include <stdint.h>
+#include <stdbool.h>
 
 typedef uint8_t Gp_Nbt_Tag_Type;
 enum {
@@ -22,46 +24,68 @@ enum {
   GP_NBT_TAG_LONG_ARRAY
 };
 
-typedef struct Gp_Nbt_Tag {
-  Gp_Nbt_Tag_Type type;
-  uint16_t nameLength;
-  char *name;
-  void *data;
-} Gp_Nbt_Tag;
+typedef struct Gp_Nbt_Tag Gp_Nbt_Tag;
 
-typedef struct {} Gp_Nbt_Tag_None_Data;
-typedef int8_t Gp_Nbt_Tag_Byte_Data;
-typedef int16_t Gp_Nbt_Tag_Short_Data;
-typedef int32_t Gp_Nbt_Tag_Int_Data;
-typedef int64_t Gp_Nbt_Tag_Long_Data;
-typedef float Gp_Nbt_Tag_Float_Data;
-typedef double Gp_Nbt_Tag_Double_Data;
-typedef struct Gp_Nbt_Tag_Byte_Array_Data {
+typedef int8_t Gp_Nbt_Tag_Byte;
+typedef int16_t Gp_Nbt_Tag_Short;
+typedef int32_t Gp_Nbt_Tag_Int;
+typedef int64_t Gp_Nbt_Tag_Long;
+typedef float Gp_Nbt_Tag_Float;
+typedef double Gp_Nbt_Tag_Double;
+
+typedef struct Gp_Nbt_Tag *Gp_Nbt_Tag_Compound;
+
+typedef struct Gp_Nbt_Tag_Byte_Array {
   int32_t size;
   uint8_t *array;
-} Gp_Nbt_Tag_Byte_Array_Data;
-typedef struct Gp_Nbt_Tag_Byte_Array_Data {
+} Gp_Nbt_Tag_Byte_Array;
+
+typedef struct Gp_Nbt_Tag_String {
   uint16_t lenght;
   char *string;
-} Gp_Nbt_Tag_Byte_Array_Data;
-typedef struct Gp_Nbt_Tag_List_Data {
+} Gp_Nbt_Tag_String;
+
+typedef struct Gp_Nbt_Tag_List {
   Gp_Nbt_Tag_Type tagType;
   int32_t lenght;
   void *payload;
-} Gp_Nbt_Tag_List_Data;
-typedef struct Gp_Nbt_Tag *Gp_Nbt_Tag_Compound_Data;
-typedef struct Gp_Nbt_Tag_Int_Array_Data {
+} Gp_Nbt_Tag_List;
+
+typedef struct Gp_Nbt_Tag_Int_Array {
   int32_t size;
-  Gp_Nbt_Tag_Int_Data *data;
-} Gp_Nbt_Tag_Int_Array_Data;
-typedef struct Gp_Nbt_Tag_Long_Array_Data {
+  Gp_Nbt_Tag_Int *data;
+} Gp_Nbt_Tag_Int_Array;
+
+typedef struct Gp_Nbt_Tag_Long_Array {
   int32_t size;
-  Gp_Nbt_Tag_Long_Data *data;
-} Gp_Nbt_Tag_Long_Array_Data;
+  Gp_Nbt_Tag_Long *data;
+} Gp_Nbt_Tag_Long_Array;
+
+struct Gp_Nbt_Tag {
+  Gp_Nbt_Tag_Type type;
+  uint16_t nameLength;
+  const char *name;
+  union Gp_Nbt_Tag_As {
+    Gp_Nbt_Tag_Byte Byte;
+    Gp_Nbt_Tag_Short Short;
+    Gp_Nbt_Tag_Int Int;
+    Gp_Nbt_Tag_Long Long;
+    Gp_Nbt_Tag_Float Float;
+    Gp_Nbt_Tag_Double Double;
+    Gp_Nbt_Tag_Byte_Array ByteArray;
+    Gp_Nbt_Tag_String String;
+    Gp_Nbt_Tag_List List;
+    Gp_Nbt_Tag_Compound Compound;
+    Gp_Nbt_Tag_Int_Array IntArray;
+    Gp_Nbt_Tag_Long_Array LongArray;
+  } as;
+};
 
 typedef Gp_Nbt_Tag Gp_Nbt;
-bool gp_is_valid_nbt(const Gp_Nbt nbt);
-Gp_Result gp_nbt_parse(void *buffer, Gp_Nbt *nbt, const Gp_Read_Byte_From_Buffer read);
-Gp_Result gp_nbt_write(void *buffer, Gp_Nbt nbt, const Gp_Write_Byte_From_Buffer write);
+
+bool gp_is_valid_nbt(Gp_Nbt nbt);
+
+Gp_Result gp_parse_nbt(void *buffer, Gp_Nbt *nbt, const Gp_Read_Byte_From_Buffer read);
+Gp_Result gp_write_nbt(void *buffer, Gp_Nbt nbt, const Gp_Write_Byte_To_Buffer write);
 
 #endif // _GPBOTLIB_NBT_H_
--- src/nbt.c
+++ src/nbt.c
@@ -1,20 +1,8 @@
-#include <stddef.h>
-
 #include <gpbotlib/nbt.h>
-#include <gpbotlib/result.h>
 
-Gp_Result _gp_create_nbt_tag(Gp_Nbt_Tag **tag) {
-  *tag = malloc(sizeof(Gp_Nbt_Tag));
-  if(!tag) return GP_BUY_MORE_RAM;
-  return GP_SUCCESS;
-}
-
-Gp_Result _gp_create_nbt_tag_and_alloc_data(Gp_Nbt_Tag **tag, size_t dataSize) {
-  *tag = malloc(sizeof(Gp_Nbt_Tag));
-  (*tag)->data = malloc(dataSize);
-  if(!tag || !(*tag)->data) return GP_BUY_MORE_RAM;
-  return GP_SUCCESS;
-}
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
 
 bool gp_is_valid_nbt(const Gp_Nbt nbt) {
   return nbt.type == GP_NBT_TAG_COMPOUND;
@@ -25,7 +13,7 @@ Gp_Result gp_nbt_parse(void *buffer, Gp_Nbt *nbt, const Gp_Read_Byte_From_Buffer
   return GP_SUCCESS;
 }
 
-Gp_Result gp_nbt_write(void *buffer, Gp_Nbt nbt, const Gp_Write_Byte_From_Buffer write) {
+Gp_Result gp_nbt_write(void *buffer, Gp_Nbt nbt, const Gp_Write_Byte_To_Buffer write) {
   assert(0 && "NOT IMPLEMENTED");
   return GP_SUCCESS;
 }
