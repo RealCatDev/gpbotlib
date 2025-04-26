65abe2cb58b8ede15c108b6b34238bbb9c414b24
CyberWordPL <cyberwordixx@gazeta.pl>
2025-04-26 20:32:34 +0200

    i need to learn how to halicunate like LLM (or smth like that)

--- src/nbt.c
+++ src/nbt.c
@@ -5,11 +5,15 @@
 
 Gp_Result _gp_create_nbt_tag(Gp_Nbt_Tag **tag) {
   *tag = malloc(sizeof(Gp_Nbt_Tag));
+  if(!tag) return GP_BUY_MORE_RAM;
+  return GP_SUCCESS;
 }
 
 Gp_Result _gp_create_nbt_tag_and_alloc_data(Gp_Nbt_Tag **tag, size_t dataSize) {
   *tag = malloc(sizeof(Gp_Nbt_Tag));
   (*tag)->data = malloc(dataSize);
+  if(!tag || !(*tag)->data) return GP_BUY_MORE_RAM;
+  return GP_SUCCESS;
 }
 
 bool gp_is_valid_nbt(const Gp_Nbt nbt) {
@@ -18,8 +22,10 @@ bool gp_is_valid_nbt(const Gp_Nbt nbt) {
 
 Gp_Result gp_nbt_parse(void *buffer, Gp_Nbt *nbt, const Gp_Read_Byte_From_Buffer read) {
   assert(0 && "NOT IMPLEMENTED");
+  return GP_SUCCESS;
 }
 
 Gp_Result gp_nbt_write(void *buffer, Gp_Nbt nbt, const Gp_Write_Byte_From_Buffer write) {
   assert(0 && "NOT IMPLEMENTED");
+  return GP_SUCCESS;
 }
